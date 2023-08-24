131a007b1dc72d280aae299c68aeb48dbc67a3c7
Rafael Mejia <devrafx@gmail.com>
2023-08-23 22:56:06 -0400

    chore: remove src folder

--- src/core/constants/ValidTypes.ts
+++
@@ -1,7 +0,0 @@
-const allowedNonPrimitives = ['Date', 'any'];
-const allowedArrayNonPrimitives = ['Date[]', 'any[]'];
-const allowedPrimitives = ['string', 'number', 'boolean', 'bigint', 'null', 'undefined']; // TODO: In the future I will check for symbol
-const allowedArrayPrimitives = ['string[]', 'number[]', 'boolean[]', 'bigint[]', 'null[]', 'undefined[]'];
-const validTypes = [...allowedPrimitives, ...allowedNonPrimitives, ...allowedArrayPrimitives, ...allowedArrayNonPrimitives];
-
-export { validTypes };
--- src/core/utils/randNumber.ts
+++
@@ -1,5 +0,0 @@
-export const rand = (max: number, min = 1) => {
-    min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
-};
